00cda49d810c99d168f4a4b2fcc5998f168ca7bf
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-09-08 19:49:21 +0300

    first commit

--- index.js
+++ index.js
@@ -25,10 +25,13 @@ const secretKey = 'your-secret-key';
 app.use(express.json())
 app.use(bodyParser.json());
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+const corsOptions = {
+    origin: ['https://bus-mkyx.onrender.com', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true,
+  };
+
+app.use(cors(corsOptions));
 
 
 app.use(cookieParser());
